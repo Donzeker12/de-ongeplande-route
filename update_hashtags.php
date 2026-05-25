33db7f0979c7f8011934d8979c5e2780cf696b06
Donzeker12 <donzeker12@example.com>
2026-05-25 16:35:45 +0200

    chore: tijdelijk update script verwijderd

--- update_hashtags.php
+++
@@ -1,6 +0,0 @@
-<?php
-require __DIR__.'/vendor/autoload.php';
-$app = require __DIR__.'/bootstrap/app.php';
-$app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-\App\Models\SiteSetting::set('instagram_hashtags', '#instagood #travel #photooftheday #familytravel #deongeplanderoute');
-echo 'OK: '.(\App\Models\SiteSetting::get('instagram_hashtags')).PHP_EOL;
